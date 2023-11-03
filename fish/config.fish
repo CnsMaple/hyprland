bcbe5fb63132283f71915a0b38ed2a46f227dd9e
CnsMaple <czy797@outlook.com>
2023-11-03 19:15:12 +0800

    日常软件更新小适配

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias nvid="neovide --multigrid"
+# alias nvid="neovide --multigrid"
+alias nvid="neovide"
+# set http proxy
+# set -x http_proxy http://127.0.0.1:7890
+# set -x https_proxy http://127.0.0.1:7890
+# set -x all_proxy socks5://127.0.0.1:7890
